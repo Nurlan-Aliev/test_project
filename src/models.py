7e5bce978bf7a0fdbd0c1b8ff3e6cf0179b090af
Nurlan-Aliev <nualiev612@gmail.com>
2025-02-02 03:15:42 +0400

    rename

--- src/models.py
+++ src/models.py
@@ -1,10 +1,11 @@
 from typing import List
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, DateTime
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 from database import Base
+from datetime import datetime, timezone
 
 
 class User(Base):
@@ -12,10 +13,13 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     fullname: Mapped[str] = mapped_column(String(30))
-    password: Mapped[str]
+    password: Mapped[str] = mapped_column(String, nullable=False)
     status: Mapped[str] = mapped_column(String(10))
-    email: Mapped[str]
-    accounts: Mapped[List['Account']] = relationship()
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    accounts: Mapped[List["Account"]] = relationship("Account", back_populates="user")
+    create_at: Mapped[DateTime] = mapped_column(
+        DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
+    )
 
 
 class Account(Base):
@@ -24,7 +28,12 @@ class Account(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     balance: Mapped[float] = mapped_column(nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    transaction: Mapped[List['Account']] = relationship()
+    transaction: Mapped[List["Account"]] = relationship(
+        "User", back_populates="accounts"
+    )
+    create_at: Mapped[DateTime] = mapped_column(
+        DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
+    )
 
 
 class Transaction(Base):
@@ -33,3 +42,6 @@ class Transaction(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     balance: Mapped[float] = mapped_column(nullable=False)
     account_id: Mapped[int] = mapped_column(ForeignKey("accounts.id"))
+    create_at: Mapped[DateTime] = mapped_column(
+        DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
+    )
